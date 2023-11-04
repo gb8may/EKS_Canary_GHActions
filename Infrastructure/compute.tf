ab301e9dfe258900ee5689b39d47ff2f13251de3
Mayara Gouveia <mayara.gouveia@soti.net>
2023-11-03 21:57:12 -0400

    Update EC2 TF code

--- Infrastructure/compute.tf
+++ Infrastructure/compute.tf
@@ -1,6 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "gh-runner" {
-  ami                         = var.ami_id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.runner_instance_type
+  tags = {
+    Name = "GH Self-hosted Runner"
+  }
+
   root_block_device           {
       volume_type = "gp3"
       volume_size = 8
