aef64f01744511a10cce59c7301d75d2389489f5
Mayara Gouveia <mayara.gouveia@soti.net>
2023-11-06 20:40:13 -0500

    Testing image scanner

--- Infrastructure/runner-userdata.sh
+++ Infrastructure/runner-userdata.sh
@@ -4,6 +4,12 @@ apt-get update
 sudo apt-get install awscli -y
 sudo apt-get install jq -y
 
+#Install Trivy
+curl -o trivy_0.46.1_Linux-64bit.tar.gz -L https://github.com/aquasecurity/trivy/releases/download/v0.46.1/trivy_0.46.1_Linux-64bit.tar.gz
+tar xzf ./trivy_0.46.1_Linux-64bit.tar.gz
+sudo mv trivy /usr/local/bin/
+sudo chmod +x /usr/local/bin/trivy
+
 # Install Docker
 sudo apt install docker.io -y
 sudo systemctl start docker
