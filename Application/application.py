3a1d7bc889e697debfc674384067279bccbeefc5
Mayara Gouveia <mayara.gouveia@soti.net>
2023-11-19 20:47:00 -0500

    Updating Webpage

--- Application/application.py
+++ Application/application.py
@@ -1,12 +1,46 @@
+# app.py
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            # Define your HTML content
+            html_content = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>EKS Deployment working!</title>
+                <style>
+                    body {
+                        font-family: 'Arial', sans-serif;
+                        background-color: #f0f0f0;
+                        text-align: center;
+                        margin-top: 100px;
+                    }
+                    h1 {
+                        color: #333;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+                <p>Welcome to my fancy webpage served by Python.</p>
+            </body>
+            </html>
+            """
+
+            # Send the HTML content as bytes
+            self.wfile.write(html_content.encode())
+        else:
+            # If the requested path is not '/', serve the default behavior
+            super().do_GET()
 
 if __name__ == '__main__':
     server_address = ('', 80)
